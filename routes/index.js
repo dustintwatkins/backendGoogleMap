67545be925e97edae6c165eab23bfe1da4da0512
dustintwatkins <dustintwatkins@gmail.com>
2017-10-28 00:45:42 +0000

    functioning front and back end OWL

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
-var express = require('express');
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.sendFile('weather.html', { root:  'public' });
 });
+
+router.get('/getcity', function(req, res, next){
+	fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
+  		if(err) throw err;
+
+		var jsonresults = [];
+
+  		var cities = data.toString().split('\n');
+		var searchString = req.query.q;
+		if(searchString)
+			var myRe = new RegExp("^" + req.query.q.toLowerCase());
+		else
+			var myRe = new RegExp("^");
+  			for(var i = 0; i < cities.length; i++) {
+    				var result = cities[i].toLowerCase().search(myRe);
+				if(result != -1){
+					console.log(cities[i]);
+					jsonresults.push({city:cities[i]});
+				}	
+  			}
+		res.status(200).json(jsonresults);
+		})
+})
+
+router.get('/getword', function(req, res, next){
+	
+	var result = [];
+
+	https.get('https://owlbot.info/api/v1/dictionary/'+ req.query.q, function(response){
+		response.on('data', function(d){
+		result += d;
+		});
+	response.on('end', function(){
+		res.status(200).json(JSON.parse( result));
+	})
+	})	
+})
 
 module.exports = router;
