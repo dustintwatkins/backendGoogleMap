cfeabbdbcdb61b1817de465972233a12f4d945b2
dustintwatkins <dustintwatkins@gmail.com>
2017-11-03 17:54:48 +0000

    working backend :)

--- routes/index.js
+++ routes/index.js
@@ -11,21 +11,19 @@ router.get('/', function(req, res, next) {
 router.get('/getTrail', function(req, res, next){
 	console.log("in getTrail router");
 	var result = [];
-	var url = 'http://api.walmartlabs.com/v1/stores?apiKey=9j6xxgnu89aaswsuz868uzk2&city=' + req.query.q +'&format=json';
+	var url = 'https://api.walmartlabs.com/v1/stores?apiKey=9j6xxgnu89aaswsuz868uzk2&city=' + req.query.q;
 	console.log(url);
 	https.get(url, function(response){
 		console.log("1");
 		response.on('data', function(d){
 		console.log("2");
 		result += d;
-	});
-	console.log("3");
-	respons.on('end', function(){
-		console.log("4");
+			});
+
+	response.on('end', function(){
 		res.status(200).json(JSON.parse(result));
+		})
 	})
-	})
-		console.log("end");
 })
 
 module.exports = router;
