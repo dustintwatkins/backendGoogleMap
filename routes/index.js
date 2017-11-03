d5dd610d2bd41fcc9ed2c9c6038dbfa9d0fac039
dustintwatkins <dustintwatkins@gmail.com>
2017-11-03 03:49:58 +0000

    Need to fix backend getTrail route

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,20 @@ router.get('/', function(req, res, next) {
 	res.sendFile('index.html', { root:  'public' });
 });
 
-router.get('getTrail', function(req, res, next){
+router.get('/getTrail', function(req, res, next){
+	console.log("in getTrail router");
 
-
-
-}
+	var result = [];
+	https.get('http://api.walmartlabs.com/v1/stores?apiKey=9j6xxgnu89aaswsuz868uzk2&city=' + req.query.q +'&format=json',
+	 function(response){
+		response.on('data', function(d){
+		result += d;
+		});
+	respons.on('end', function(){
+		res.status(200).json(JSON.parse(result));
+	})
+	})
+		
+});
 
 module.exports = router;
